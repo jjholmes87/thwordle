73c081eeb341c56ef634d1473d3ce4c97c18d49f
JHolmes87 <64210391+JHolmes87@users.noreply.github.com>
2024-09-27 14:59:48 +0700

    changed Twitter and FB URLs

--- src/lib/Social.svelte
+++ src/lib/Social.svelte
@@ -7,7 +7,7 @@
 
 {#if url}
   <div class="flex gap-2 justify-center w-full fixed bottom-4 center">
-    <Facebook class="h-10 w-10 rounded" {url} />
-    <Twitter class="h-10 w-10 rounded" text={title} {url} />
+    <Facebook class="h-10 w-10 rounded" url="https://xn--l3cap7c4a4br8ewf.fun/" />
+    <Twitter class="h-10 w-10 rounded" text={title} url="https://xn--l3cap7c4a4br8ewf.fun/" />
   </div>
 {/if}
